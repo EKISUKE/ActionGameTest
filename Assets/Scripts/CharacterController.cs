9596e841d56f89c537c3b7dd95da634f8fdbdb54
EKISUKE <yuuki622800@yahoo.co.jp>
2019-03-02 18:13:11 +0900

    アクションゲームのテスト
    UnityChanがマウスクリックで
    3つのアニメーションを遷移するだけのシーン作成途中

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// テスト的なキャラクターコントロール
+/// </summary>
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(InputController))]
+public class CharacterController : MonoBehaviour
+{
+    protected Animator        animator = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        var inputController = GetComponent<InputController>();
+        if(inputController)
+        {
+            inputController.RegisterDelegateWithControllerInfo(UpdateFromInputController);
+        }
+    }
+
+    void OnDestroy()
+    {
+        var inputController = GetComponent<InputController>();
+        if (inputController)
+        {
+            inputController.UnRegisterDelegateWithControllerInfo(UpdateFromInputController);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateFromInputController(InputController.InputInfo inputInfo)
+    {
+        if (!animator) return;
+        if (inputInfo.currentBit.Get(0))
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 入力からCharacterControllerに情報を通知する
+/// </summary>
+public class InputController : MonoBehaviour
+{
+    protected const int bitBufferCount = 2;
+    public class InputInfo
+    {
+        public List<BitArray> controllerBits = new List<BitArray>(bitBufferCount);
+        public Vector2 LStick { get; set; }
+        public Vector2 RStick { get; set; }
+        public BitArray currentBit
+        {
+            get
+            {
+                return controllerBits[GetCurrentBufferIndex()];
+            }
+        }
+        public BitArray prevBit
+        {
+            get
+            {
+                return controllerBits[GetPrevBufferIndex()];
+            }
+        }
+
+
+        public InputInfo()
+        {
+            for (int i = 0; i < controllerBits.Capacity; ++i)
+            {
+                controllerBits.Add(new BitArray(64));
+            }
+        }
+
+        int GetCurrentBufferIndex()
+        {
+            return Time.frameCount % bitBufferCount;
+        }
+
+        int GetPrevBufferIndex()
+        {
+            return (bitBufferCount + (GetCurrentBufferIndex() - 1)) % bitBufferCount;
+        }
+
+        public void UpdateControllerInfo()
+        {
+            var currentBuffer = GetCurrentBufferIndex();
+            controllerBits[currentBuffer].Set(0, Input.GetButtonDown("Fire1"));
+        }
+    };
+
+
+    /// <summary>
+    /// ビット更新後のビットを使った処理呼び出し
+    /// </summary>
+    /// <param name="controllerBits"></param>
+    public delegate void DoSomethingWithBits(InputInfo inputInfo);
+
+    protected InputInfo inputInfo = null;
+
+    protected DoSomethingWithBits delegateDoSomething = null;
+
+    public void RegisterDelegateWithControllerInfo(DoSomethingWithBits function)
+    {
+        delegateDoSomething += function;
+    }
+
+    public void UnRegisterDelegateWithControllerInfo(DoSomethingWithBits function)
+    {
+        delegateDoSomething -= function;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputInfo = new InputInfo();
+    }
+
+    
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputInfo.UpdateControllerInfo();
+        delegateDoSomething?.Invoke(inputInfo);
+    }
+}
