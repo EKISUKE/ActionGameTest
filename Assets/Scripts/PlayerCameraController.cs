044376e889fabd3850f22daca4ef80ba378b2da8
EKISUKE <yuuki622800@yahoo.co.jp>
2019-03-04 00:15:49 +0900

    カメラ角度制限追加

--- Assets/Scripts/PlayerCameraController.cs
+++ Assets/Scripts/PlayerCameraController.cs
@@ -10,15 +10,15 @@ public class PlayerCameraController : MonoBehaviour
 {
     public class CameraTransform
     {
-        public Vector3 addEulerAngle;
+        public Vector3    eulerAngle;
         public Quaternion vRotation;
         public Quaternion hRotation;
 
         public CameraTransform(Transform transform)
         {
-            addEulerAngle = transform.rotation.eulerAngles;
-            vRotation = Quaternion.Euler(addEulerAngle.x, 0.0f, 0.0f);
-            hRotation = Quaternion.Euler(0.0f, addEulerAngle.y, 0.0f);
+            eulerAngle = transform.rotation.eulerAngles;
+            vRotation = Quaternion.Euler(eulerAngle.x, 0.0f, 0.0f);
+            hRotation = Quaternion.Euler(0.0f, eulerAngle.y, 0.0f);
         }
 
     };
@@ -27,11 +27,12 @@ public class PlayerCameraController : MonoBehaviour
 
     [Tooltip("注視対象")]
     public Transform       forcusTrans = null;
-
     [Tooltip("注視点からの距離")]
     public float           length = 2.0f;
-
+    [Tooltip("現在目標とする注視点")]
     protected Vector3      currentForcusPos = Vector3.zero;
+    [Tooltip("入力によって回転角度が変更されたかどうか")]
+    protected bool         isChangeRotateByInput   = false;
 
     // Start is called before the first frame update
     void Start()
@@ -72,24 +73,22 @@ public class PlayerCameraController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        camTrans.hRotation *= Quaternion.Euler(0.0f,                  camTrans.addEulerAngle.y, 0.0f);
-        camTrans.vRotation *= Quaternion.Euler(camTrans.addEulerAngle.x, 0.0f,                  0.0f);
-
         // 注視点 + Y軸1m
         var forcusPos = forcusTrans.position + Vector3.up;
         var targetVec = forcusPos - this.transform.position;
-        if (camTrans.addEulerAngle.magnitude > 0.0f) {
-            this.transform.rotation = camTrans.hRotation * camTrans.vRotation;
-        }
-        else
-        {
+        ref var euler     = ref camTrans.eulerAngle;
+        // 入力による回転角度変更がない場合
+        if (!isChangeRotateByInput) {
             // ターゲット方向への回転角度を求める
-            this.transform.rotation = Quaternion.LookRotation(targetVec.normalized);
-            var euler = this.transform.rotation.eulerAngles;
-            camTrans.hRotation = Quaternion.Euler(0.0f, euler.y, 0.0f);
-            camTrans.vRotation = Quaternion.Euler(euler.x, 0.0f, 0.0f);
+            euler = Quaternion.LookRotation(targetVec.normalized).eulerAngles;
+            euler.x = euler.x - 360.0f < -180.0f ? euler.x : euler.x - 360.0f;
         }
-        
+        // 回転角度制限
+        euler.x = Mathf.Clamp(euler.x, -50.0f, 70.0f);
+
+        camTrans.hRotation = Quaternion.Euler(0.0f, euler.y, 0.0f);
+        camTrans.vRotation = Quaternion.Euler(euler.x, 0.0f, 0.0f);
+        this.transform.rotation = camTrans.hRotation * camTrans.vRotation;
         currentForcusPos = forcusPos;
         var targetPos = CalclateTargetPos(currentForcusPos, 1.0f);
 
@@ -99,7 +98,18 @@ public class PlayerCameraController : MonoBehaviour
     void UpdateFromInputController(InputController.InputInfo inputInfo)
     {
         var factor = 100.0f;
-        camTrans.addEulerAngle.y = inputInfo.RStick.x * factor * Time.deltaTime;
-        camTrans.addEulerAngle.x = inputInfo.RStick.y * factor * Time.deltaTime;
+        if (inputInfo.RStick.magnitude > 0.0f)
+        {
+            camTrans.eulerAngle.y += inputInfo.RStick.x * factor * Time.deltaTime;
+            camTrans.eulerAngle.x += inputInfo.RStick.y * factor * Time.deltaTime;
+            camTrans.eulerAngle.x = Mathf.Clamp(camTrans.eulerAngle.x, -80.0f, 80.0f);
+            isChangeRotateByInput = true;
+        }
+        else
+        {
+            isChangeRotateByInput = false;
+        }
+
+        
     }
 }
